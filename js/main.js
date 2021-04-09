17335508ed59f10dde2d75aed2737c046c9c742a
jtao7 <jtao7@artic.edu>
2021-04-10 02:43:27 +0900

    Shader on 3d object

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// a shader variable
+let theShader;
+// let shaderTexture;
+
+
+
+function preload(){
+  // load the shader
+  theShader = loadShader('shader.vert', 'shader.frag');
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(innerWidth, innerHeight, WEBGL);
+  noStroke();
+  // // initialize the createGraphics layers
+  // shaderTexture = createGraphics(710, 400, WEBGL);
+  //
+  // // turn off the createGraphics layers stroke
+  // shaderTexture.noStroke();
+  // x = -50;
+  //  y = 0;
+}
+
+function draw() {
+  background(0);
+  // shader() sets the active shader with our shader
+  //shaderTexture.
+  shader(theShader);
+
+  theShader.setUniform("u_resolution", [width, height]);
+  theShader.setUniform("u_time", millis() / 1000.0);
+  theShader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)]);
+
+  // // rect gives us some geometry on the screen
+  // shaderTexture.rect(0,0,width, height);
+
+
+  //pass the shader as a texture
+  // texture(shaderTexture);
+  // translate(-150, 0, 0);
+  //   push();
+  //   rotateZ(theta * mouseX * 0.0001);
+  //   rotateX(theta * mouseX * 0.0001);
+  //   rotateY(theta * mouseX * 0.0001);
+  //   theta += 0.05;
+  // rect(0,0,width,height)
+  rotateX(0.3)
+  box(width/4)
+  // pop();
+
+  // //let ellipseFidelity = int(map(mouseX, 0, width, 8, 100));
+  //   ellipse(260, 0, 200, 200, ellipseFidelity);
+  }
+// function windowResized(){
+//   resizeCanvas(windowWidth, windowHeight);
+// }
---
+++ shader.vert
@@ -0,0 +1,27 @@
+// vert file and comments from adam ferriss
+// https://github.com/aferriss/p5jsShaderExamples
+
+// our vertex data
+attribute vec3 aPosition;
+
+attribute vec2 aTexCoord;
+uniform mat4 uProjectionMatrix;
+uniform mat4 uModelViewMatrix;
+varying vec2 vTexCoord;
+
+void main() {
+
+  vec4 positionVec4 = vec4(aPosition, 1.0);
+  gl_Position = uProjectionMatrix * uModelViewMatrix * positionVec4;
+
+  vTexCoord = aTexCoord;
+
+
+  // vTexCoord = aTexCoord;
+  // // copy the position data into a vec4, using 1.0 as the w component
+  // vec4 positionVec4 = vec4(aPosition, 1.0);
+  // positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+
+  // // send the vertex information on to the fragment shader
+  // gl_Position = positionVec4;
+}
